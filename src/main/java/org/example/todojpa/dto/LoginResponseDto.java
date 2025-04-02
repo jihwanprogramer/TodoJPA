0e82573fa38bbaaa61621e139fcdbbbe89621382
jihwanprogramer <kob882333@naver.com>
2025-04-02 14:12:49 +0900

    로그인 응답 dto

---
+++ src/main/java/org/example/todojpa/dto/LoginResponseDto.java
@@ -0,0 +1,11 @@
+package org.example.todojpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+
+    private final Long id;
+}
