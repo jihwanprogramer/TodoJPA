7d4e60d401ffe922cf71c03d60c792519e0529fb
jihwanprogramer <kob882333@naver.com>
2025-04-02 14:11:56 +0900

    로그인 필터추가

---
+++ src/main/java/org/example/todojpa/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.example.todojpa.config;
+
+import jakarta.servlet.Filter;
+import org.example.todojpa.filter.LoginFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
---
+++ src/main/java/org/example/todojpa/filter/LoginFilter.java
@@ -0,0 +1,35 @@
+package org.example.todojpa.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {"/","/users/signup1","/login","/logout"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if(isWhiteList(requestURI)){
+            HttpSession session = httpRequest.getSession(false);
+            if (session == null || session.getAttribute("sessionKey") == null){
+                throw new RuntimeException("로그인 해주세요");
+            }
+        }
+
+        chain.doFilter(request,response);
+    }
+
+    private boolean isWhiteList(String requestURI){
+        return PatternMatchUtils.simpleMatch(WHITE_LIST,requestURI);
+    }
+}
