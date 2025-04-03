86d3e437bf8da0c45cc38215ce3141b18f0721e8
jihwanprogramer <kob882333@naver.com>
2025-04-03 12:32:00 +0900

    fix(LoginService): 로그인 로직 수정
    로그인 실패 시 예외 처리를 개선하여 사용자에게 더 명확한 메시지를 전달합니다.

--- src/main/java/org/example/todojpa/service/LoginService.java
+++ src/main/java/org/example/todojpa/service/LoginService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LoginService {
+
     private final UserRepository userRepository;
 
     public LoginResponseDto login(String email, String password) {
@@ -19,5 +20,14 @@ public class LoginService {
         }
 
         return new LoginResponseDto(user.getId());
+    }
+
+    public String getEmail(String email) {
+        if(userRepository.hasSameEmail(email))
+        {
+            return "이미 존재하는 이메일 입니다";
+        }else{
+            return "사용 가능한 이름 입니다";
+        }
     }
 }
