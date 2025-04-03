1aac23f1fc98f131167782daec61460ce996772d
jihwanprogramer <kob882333@naver.com>
2025-04-03 12:31:51 +0900

    feat(LoginRequestDto): 유효성 검사 추가
    이메일과 비밀번호 필드에 대한 유효성 검사를 추가했습니다.

--- src/main/java/org/example/todojpa/dto/LoginRequestDto.java
+++ src/main/java/org/example/todojpa/dto/LoginRequestDto.java
@@ -1,5 +1,8 @@
 package org.example.todojpa.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +10,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class LoginRequestDto {
 
-    private final String email;
+    @NotBlank(message = "이메일 입력은 필수 입니다.")
+    @Email
+    private String email;
 
+    @Size(min = 8,message = "비밀번호는 최소 8글자 이상이여야 합니다")
     private final String password;
 }
