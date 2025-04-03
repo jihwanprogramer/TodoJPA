70a2eb2c805f9349b2dc617ecad87cf3b1200679
jihwanprogramer <kob882333@naver.com>
2025-04-03 12:32:38 +0900

    feat(LoginController): 로그인 요청에 대한 기능 추가

--- src/main/java/org/example/todojpa/controller/LoginController.java
+++ src/main/java/org/example/todojpa/controller/LoginController.java
@@ -2,6 +2,9 @@ package org.example.todojpa.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.example.todojpa.common.Const;
 import org.example.todojpa.dto.LoginRequestDto;
@@ -11,8 +14,10 @@ import org.example.todojpa.service.LoginService;
 import org.example.todojpa.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
@@ -21,18 +26,28 @@ public class LoginController {
     private final UserService userService;
 
     @PostMapping("/session-login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto dto, HttpServletRequest request){
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto dto, HttpServletRequest request){
         LoginResponseDto loginResponseDto = loginService.login(dto.getEmail(), dto.getPassword());
 
         if(loginResponseDto.getId()==null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        HttpSession session = request.getSession();
 
+        HttpSession session = request.getSession();
         UserResponseDto loginUser = userService.findById(loginResponseDto.getId());
         session.setAttribute(Const.LOGIN_USER, loginUser);
 
         return new ResponseEntity<>(loginResponseDto,HttpStatus.OK);
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<String> check(
+            @NotBlank(message = "이메일 입력은 필수 입니다.")
+            @Email
+            @RequestParam String email){
+        String result = loginService.getEmail(email);
+
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
 
     @PostMapping("/session-logout")
